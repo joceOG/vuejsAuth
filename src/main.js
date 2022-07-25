0209e554db2c27012a41461319232bb2ddfe3dac
BOKA KOFFI JOCELYN <jocelyn.boka@uniskip.com>
2022-07-25 20:17:24 +0100

    My first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bulma/css/bulma.css";
+
+// Import the Auth0 configuration
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+const domain = "dev-6vq4z-1l.us.auth0.com";
+const clientId = "FL7eMedS9EBrXI8YMtvz1wBteeb0Jnh9";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
 
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import { authGuard } from "../auth/authGuard";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    name: "about",
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/event/:id",
+    name: "eventSingle",
+    component: () => import("../views/EventSingle.vue"),
+    beforeEnter: authGuard,
   },
 ];
 
